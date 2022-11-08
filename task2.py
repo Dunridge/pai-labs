84046d5bc2390938c0b1772232ec471f983ae007
Dunridge <Polinka7max@gmail.com>
2022-11-08 18:23:19 +0200

    develop. added task 2

--- task2.py
+++ task2.py
@@ -17,6 +17,29 @@ eps = input("Please provide your epsilon").
 
 '''
 
-# TODO: figure out what does the g stand for
-
 #   g is the limit of a sequence (for each series 1/n, g = 0 1 + 1/n g = e (n)^(1/n)=1)
+
+target_n = 0
+
+target_n_is_smaller = True
+eps = float(input("Please provide your epsilon: "))
+
+n = 1
+g = 1   # for this series the value is always 1
+N = 0
+
+while target_n_is_smaller:
+    func = 1 + 1/n**(3/2)
+
+    if abs(func - g) > eps:
+        N = n
+        print('set to false')
+        target_n_is_smaller = False
+
+    n += 1
+
+
+print("N: ", N)
+
+
+
